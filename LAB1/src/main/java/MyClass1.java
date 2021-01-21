3761b74d645d4704fff7c45a33f54e56f16ce0a7
Yash1632 <yash160203@gmail.com>
2021-01-21 20:59:14 +0530

    .jar File Build using maven

---
+++ LAB1/src/main/java/MyClass1.java
@@ -0,0 +1,15 @@
+
+public class MyClass1 {
+	
+	public void sum(int a, int b) {
+		int s = a+b;
+		System.out.println("Sum Is: "+s);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass1 obj = new MyClass1();
+		obj.sum(12, 13);
+	}
+
+}
